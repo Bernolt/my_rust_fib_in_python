84b33bee33728384191afed325ef595d0c7d31e8
Bernard Jesop <bernard.jesop@gmail.com>
2022-06-25 11:06:27 +0200

    add Adapter Counter

--- src/lib.rs
+++ src/lib.rs
@@ -16,9 +16,9 @@ fn say_hello() {
 
 #[pymodule]
 fn flitton_fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_wrapped(wrap_pyfunction!(say_hello));
-    m.add_wrapped(wrap_pyfunction!(fibonacci_number));
-    m.add_wrapped(wrap_pyfunction!(fibonacci_numbers));
+    m.add_wrapped(wrap_pyfunction!(say_hello))?;
+    m.add_wrapped(wrap_pyfunction!(fibonacci_number))?;
+    m.add_wrapped(wrap_pyfunction!(fibonacci_numbers))?;
     
     Ok(())
 }
