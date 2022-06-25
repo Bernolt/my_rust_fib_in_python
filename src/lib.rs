b1efab3dd88e1f0de78f35c521e1c764f8756dac
Bernard Jesop <bernard.jesop@gmail.com>
2022-06-25 10:20:36 +0200

    add fib_calc module

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,23 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
+mod fib_calcs;
+
 #[pyfunction]
 fn say_hello() {
     println!("saying hello from Rust!");
 }
 
+use fib_calcs::fib_number::__pyo3_get_function_fibonacci_number;
+use fib_calcs::fib_numbers::__pyo3_get_function_fibonacci_numbers;
+
+// pub mod fib_numbers;
+
 #[pymodule]
 fn flitton_fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(say_hello));
+    m.add_wrapped(wrap_pyfunction!(fibonacci_number));
+    m.add_wrapped(wrap_pyfunction!(ibonacci_numbers));
+    
     Ok(())
 }
